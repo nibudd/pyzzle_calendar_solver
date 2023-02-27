5e70812810863fafa4b77cedff90668a56a350b0
nibudd <nibudd@gmail.com>
2023-02-26 20:17:57 -0500

    started building Square class

---
+++ src/square.py
@@ -0,0 +1,31 @@
+import pickle
+
+
+class Square(tuple):
+    def __init__(self, tup: tuple) -> "Square":
+        if len(tup) != 2:
+            raise InvalidSquareError(f"Invalid tuple length of {len(tup)}. Tuple must be of length 2.")
+
+        if any(not isinstance(val, int) for val in tup):
+            raise InvalidSquareError("Invalid values int tuple. All values must be `int`s")
+
+        super().__init__()
+    
+    @property
+    def row(self) -> int:
+        return self[0]
+
+    @property
+    def col(self) -> int:
+        return self[1]
+
+    @property
+    def hash(self) -> str:
+        return pickle.dumps(tuple(self))
+
+    def reflect(self) -> "Square":
+        return Square((-self.row, self.col))
+
+
+class InvalidSquareError(Exception):
+    pass
---
+++ test/unit/square/test_square.py
@@ -0,0 +1,50 @@
+from typing import Any
+
+import pytest
+
+from src.square import Square, InvalidSquareError
+
+
+def test_row_returns_element_0():
+    sut = Square((0, 1))
+    assert sut.row == 0
+
+
+def test_col_returns_element_1():
+    sut = Square((0, 1))
+    assert sut.col == 1
+
+
+def test_hashes_of_Square_with_same_tuple_are_equal():
+    sut_1 = Square((0, 1))
+    sut_2 = Square((0, 1))
+
+    assert sut_1.hash == sut_2.hash
+
+
+def test_hashes_of_Square_with_different_tuples_are_not_equal():
+    sut_1 = Square((0, 1))
+    sut_2 = Square((4, 4))
+
+    assert sut_1.hash != sut_2.hash
+
+
+@pytest.mark.parametrize("tup", [
+    (),
+    (1,),
+    (3, 3, 3),
+    (4, 4, 4, 4)
+])
+def test_InvalidSquareError_raised_for_tuples_of_length_other_than_2(tup: tuple[int]):
+    with pytest.raises(InvalidSquareError):
+        Square(tup)
+
+
+@pytest.mark.parametrize("tup", [
+    (1, 1.1),
+    ("hi", 2, 3),
+    (1, 2, True)
+])
+def test_tuples_containing_non_int_values_raise_InvalidSquareError(tup: tuple[Any]):
+    with pytest.raises(InvalidSquareError):
+        Square(tup)
